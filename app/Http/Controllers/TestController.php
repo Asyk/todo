a1dd051b59d505e3f269554bd2c9b4f38179423e
Quattro43523 <sarkytbekbekzhan80@gmail.com>
2023-11-07 23:40:25 +0600

    crud beka test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class TestController extends Controller
 {
     public function bekatest()
     {
-         echo "<h1>Hello World</h1>";
+        // $user = User::create([
+        //     'name' => 'Beka',
+        //     'email' => 'beka@gmail.com',
+        //     'password' => 'asdfasdf',
+        // ]);
+
+        $users = User::where('id', 1) -> first();
+        if($users){
+
+         $users -> update([
+
+            'name' => 'eridan',
+            'email' => 'eridanus@gmail.com',
+
+         ]);
+
+        }
+
+        $users = User::all();
+        dd($users->toArray());
+
+
     }
+
 }
